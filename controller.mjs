0b3fb2ec3632c7354ee1ecf029d2b62baa60a751
Steve Rector <steverector27@gmail.com>
2023-05-31 13:04:40 -0700

    Confirm Post requests working as intended

--- controller.mjs
+++ controller.mjs
@@ -4,4 +4,23 @@ import * as games from './model.mjs';
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(express.json());  // REST needs JSON MIME type.
+app.use(express.json());  
+
+
+app.post('/games', (req, res) => {
+    games.createGame(
+        req.body.title,
+        req.body.timeToBeat,
+        req.body.releaseDate
+    )
+    .then(game => {
+        res.status(201).json(game)
+    })
+    .catch(error => {
+        res.status(400).json({error: "create a document failed"});
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
