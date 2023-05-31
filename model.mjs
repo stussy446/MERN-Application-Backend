8912e02e296b133b9d44a171dc717b34d551305a
Steve Rector <steverector27@gmail.com>
2023-05-31 12:05:55 -0700

    Add Create, Retrieve, and Update methods to Game controller

--- model.mjs
+++ model.mjs
@@ -13,7 +13,7 @@ db.once("open", (err) => {
     if(err){
         res.status(500).json({ error: '500:Connection to the server failed.' });
     } else  {
-        console.log('Successfully connected to MongoDB Games Backlog collection using Mongoose.');
+        console.log('Successfully connected to MongoDB Games Beaten collection using Mongoose.');
     }
 });
 
@@ -25,5 +25,42 @@ const gameSchema = mongoose.Schema({
 });
 
 // Compile the model from the schema.
-const game = mongoose.model("Game", gameSchema)
+const Game = mongoose.model("Game", gameSchema)
 
+// Creates new Game document 
+const createGame = async (title, timeToBeat, releaseDate) => {
+    const game = new Game({
+        title: title,
+        timeToBeat: timeToBeat,
+        releaseDate: releaseDate
+    });
+
+    return game.save();
+};
+
+// Retrieves all Game documents 
+const retrieveGames = async () => {
+    const query = Game.find();
+    return query.exec();
+};
+
+// Retrieves specific Game document by ID
+const retrieveGameByID = async (_id) => {
+    const query = Game.findById({_id: _id})
+    return query.exec();
+};
+
+const updateGame = async (_id, title, timeToBeat, releaseDate) => {
+    const result = await Game.replaceOne({_id: _id}, {
+        title: title,
+        timeToBeat: timeToBeat,
+        releaseDate: releaseDate
+    });
+
+    return {
+        _id: _id,
+        title: title,
+        timeToBeat: timeToBeat,
+        releaseDate: releaseDate
+    }
+};
